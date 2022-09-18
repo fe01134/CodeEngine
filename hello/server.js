196dc5e6ffade287d23f3e7ffbe7a6318ebeb17c
Carlos Ferreira <carlosyells@yahoo.com>
2022-09-18 18:42:26 +0100

    Update server.js
    testing modification save, build and deploy

--- hello/server.js
+++ hello/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 http.createServer(function (request, response) {
-   target = process.env.TARGET ? process.env.TARGET : 'World' ;
-   msg = process.env.MSG ? process.env.MSG : 'Hello there' + target + '\n';
+   target = process.env.TARGET ? process.env.TARGET : 'World Now' ;
+   msg = process.env.MSG ? process.env.MSG : 'Say Hello there' + target + '\n';
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end(msg);
 }).listen(8080);
